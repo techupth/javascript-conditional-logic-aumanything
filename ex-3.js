72b016677006780cf6e26acfb16d857a8e403c3b
thiranattada <thiranattada.n@muze.co.th>
2024-06-14 17:20:38 +0700

    submit assignment javascript-conditional-logic

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,13 @@
 //Exercise 3
 // Ternary Operator
-let lightBulbStatus = "Off";
+// ให้นำคำตอบจากโจทย์ Exercise 1 มาเขียนใหม่ในรูปแบบ Ternary Operator
+// กรณีที่ไม่เข้าเงื่อนไขใด ๆ เลยให้โปรแกรมหลอดไฟแสดงข้อความว่า “Please choose the correct input (On/Off)” ออกมาทางหน้าจอด้วย console.log()
+// if (lightBulbStatus === "On")
+//     {console.log("Light bulb is On.");} 
+// else {console.log("Light bulb is Off.");}
+
+
+let lightBulbStatus = "Broken";
+lightBulbStatus === "On"?(console.log("Light bulb is On.")): lightBulbStatus === "Off"?(console.log("Light bulb is Off.")):console.log("Please choose the correct input (On/Off)")
 
 //Start coding here
